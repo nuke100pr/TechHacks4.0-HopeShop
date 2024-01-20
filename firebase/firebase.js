fc3935c881e5b02c3580185bb6531594232670c1
Ark898 <aryankumar89824@gmail.com>
2024-01-21 00:50:29 +0530

    Files added

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -21,12 +21,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
+  apiKey: "AIzaSyA14cd6cJjIRU3Ow7e_ixhIOHKn7AcKiTU",
+  authDomain: "hopeshop-67303.appspot.com",
+  projectId: "hopeshop-67303",
+  storageBucket: "hopeshop-67303.appspot.com",
+  messagingSenderId: "87845754589",
+  appId:"1:87845754589:android:1358bce30eb2b66ea12d34",
 };
 
 const app = initializeApp(firebaseConfig);
